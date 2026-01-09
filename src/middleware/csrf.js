772f80ad6902c75ecfd71201795c0aea4d2fbfa6
Jaime Carrasco <JaimeCarrasco2name>
2026-01-09 02:31:31 +0100

    Implementacion de inicio de sesion y proteccion de CSRF 2.0

--- src/middleware/csrf.js
+++ src/middleware/csrf.js
@@ -1,3 +1,4 @@
+//CSRF es para proteger contra ataques
 const csurf = require('csurf');
 const csrfProtection = csurf({ cookie: false });
 module.exports = csrfProtection;
--- src/middleware/requireLogin.js
+++ src/middleware/requireLogin.js
@@ -1,3 +1,4 @@
+//Esto es para iniciar sesión y proteger las rutas que requieren autenticación
 function requireLogin(req, res, next) {
   if (req.session && req.session.user) {
     next();
